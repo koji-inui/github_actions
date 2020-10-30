c2f75af1181c30e23a4f2d7b457d0cd65587b641
koji-inui <koji.inui77@gmail.com>
2020-10-30 22:28:13 +0900

    test raises

--- tests/test_say.py
+++ tests/test_say.py
@@ -1,10 +1,16 @@
+import pytest 
 from main.say import Foo,Hoge
 
 def test_foo_say():
     assert Foo().say() == 'foo'
 
+def test_foo_say_typeerror():
+    with pytest.raises(TypeError):
+        Foo().say(12) 
+
 def test_foo_say2():
-    assert Foo().say2() == 'foo'  #テストエラーの出力確認。
+    assert Foo().say2() != 'foo' 
+    assert Foo().say2() == 'foo2'  
 
 def test_hoge_say():
     assert Hoge().say() == 'hoge'
